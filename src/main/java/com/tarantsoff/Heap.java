4eeaf59dcdfea96f974f6d0a87c6d7b11311dd13
tarantsoff <tarantsoff@gmail.com>
2019-09-11 23:08:50 +0300

    a lot of demos and garbage

---
+++ src/main/java/com/tarantsoff/Heap.java
@@ -0,0 +1,21 @@
+package com.tarantsoff;
+
+import java.util.LinkedHashMap;
+
+public class Heap {
+
+    public static void main(String[] args) {
+
+        LinkedHashMap<String, String> userCityMapping = new LinkedHashMap<>();
+
+        userCityMapping.put("Rajeev", "Bengaluru");
+        userCityMapping.put("Chris", "London");
+        userCityMapping.put("David", "Paris");
+        userCityMapping.put("Jesse", "California");
+
+        System.out.println("=== Iterating over a LinkedHashMap using Java 8 forEach and lambda ===");
+        userCityMapping.forEach((user, city) -> {
+            System.out.println(user + " => " + city);
+        });
+    }
+}
